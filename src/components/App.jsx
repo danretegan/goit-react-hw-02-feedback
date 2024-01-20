e9a16396fe7023ee08f317b841b36b130ee7eb4b
Dan Retegan <danretegan@yahoo.com>
2024-01-20 16:43:50 +0200

    Counter.jsx disbanded.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,49 @@
 // App.jsx
-
-import Counter from './Counter';
+import React, { Component } from 'react';
 import styles from './App.module.css';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
 
-export const App = () => {
-  return (
-    <div className={styles.container}>
-      <p className={styles.title}>Please leave a feedback</p>
-      <Counter />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  handleIncrement = buttonName => {
+    this.setState(prevState => ({
+      [buttonName]: prevState[buttonName] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = good + neutral + bad;
+    const positiveFeedbackPercentage =
+      totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+
+    return (
+      <div className={styles.container}>
+        <p className={styles.title}>Please leave a feedback</p>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.handleIncrement}
+        />
+
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,26 @@
+// Statistics.jsx
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <>
+    <p>
+      <strong>Statistics:</strong>
+    </p>
+    <div>Good: {good}</div>
+    <div>Neutral: {neutral}</div>
+    <div>Bad: {bad}</div>
+    <div>Total: {total}</div>
+    <div>Positive feedback: {positivePercentage}%</div>
+  </>
+);
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
+
+export default Statistics;
